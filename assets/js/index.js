bc4dd191744daefff5cbfc984473182d1943b2df
Jorge Lopez <jorge_lopezd@hotmail.com>
2023-07-14 11:59:51 -0400

    se agrega funcion del boton

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+const colorselect = document.querySelector('.selectcolor');
+const btntotal = document.querySelector('.btntotal');
+const cantidadselect = document.querySelector('#cantidad');
+
+const colorinfo = document.querySelector('.color-info');
+const cantidadinfo = document.querySelector('.cantidad-info');
+const totalinfo = document.querySelector('.total-info');
+
+btntotal.addEventListener('click', function() {
+    const cantidad = cantidadselect.value;
+    const color = colorselect.value;
+    const total = calculartotal(cantidad);
+
+    totalinfo.textContent = `Total: ${total}`;
+    cantidadinfo.textContent = `Cantidad: ${cantidad}`;
+    colorinfo.textContent = `Color: ${color}`;
+});
+
+function calculartotal(cantidad) {
+    const preciounitario = 400000;
+    return cantidad * preciounitario;
+}
